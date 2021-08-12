58aa3f476a3e70245d4755165fc45ef9ba3b0fbc
ytyaru <yttry0@gmail.com>
2021-08-12 15:15:12 +0900

    #            'Content-Type': 'application/json',

--- src/toot.py
+++ src/toot.py
@@ -53,7 +53,6 @@ class Api:
     def Header(self):
         return {
             'User-Agent': 'Mozilla/5.0 (X11; CrOS armv7l 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187 Safari/537.36',
-            'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.Auth.Token}',
         }
         
