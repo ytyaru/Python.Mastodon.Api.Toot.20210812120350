e0ac5b07c9402828b7f95ee6a6cd3aa1ae225614
ytyaru <yttry0@gmail.com>
2021-08-12 15:18:01 +0900

    User-Agentも消した。成功確認済み。

--- src/toot.py
+++ src/toot.py
@@ -52,7 +52,8 @@ class Api:
     @property
     def Header(self):
         return {
-            'User-Agent': 'Mozilla/5.0 (X11; CrOS armv7l 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187 Safari/537.36',
+#            'User-Agent': 'Mozilla/5.0 (X11; CrOS armv7l 13597.84.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.187 Safari/537.36',
+#            'Content-Type': 'application/json',
             'Authorization': f'Bearer {self.Auth.Token}',
         }
         
@@ -72,7 +73,7 @@ class Toot(Api):
 
 if __name__ == "__main__":
     toot = Toot()
-    content = '''PythonでAPI を叩いてみた。
+    content = '''User-Agentを消してみる。
 #mastodon #api'''
     print(json.dumps(toot.toot(content)))
 
